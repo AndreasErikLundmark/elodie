2fc7bcbcb256b5137df45435c36f32a19c5c960d
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-02-08 15:32:01 +0100

    bg main

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
+import bg from '../src/assets/images/atendbg.png'
 
 function App() {
+  const coverAttheEnd = {
+    backgroundImage: `url(${bg})`, 
+    backgroundSize: 'cover', 
+    backgroundPosition: 'center', 
+  };
 
 
   return (
     <>
-    <div className="header flex justify-center items-center">
-    <h1 className="text-green-500">élodie</h1>
-    </div>
-  
+     <div className="w-full h-screen bg-white"> 
+
+  <div className="header flex justify-center items-center">
+    <h1 className="text-black">élodie</h1> {/* Text is white to stand out on green background */}
+  </div>
+  <div className="main flex justify-center items-center h-full w-full">
+    {/* The main content */}
+  </div>
+</div>
 
 
     </>
