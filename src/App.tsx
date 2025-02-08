161ace24e817c146104a0257d53380d610221540
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-02-08 15:13:54 +0100

    vite and daisyui setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className="header flex justify-center items-center">
+    <h1 className="text-green-500">élodie</h1>
+    </div>
+  
+
+
+    </>
+  )
+}
+
+export default App
