9c600ae4eef104ad9c68882c96a6b7433c511044
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-02-10 09:35:55 +0100

    prettier

--- src/assets/birds/birds.tsx
+++ src/assets/birds/birds.tsx
@@ -1,29 +1,33 @@
-
-
 export default function Birds({ isPlaying }) {
- 
-    return(
-        <div>
-        {/* Bird container 1 */}
-        <div className={`bird-container bird-container-one ${isPlaying ? 'animate' : ''}`}>
-          <div className="bird bird-one"></div>
-        </div>        
-        
-        {/* Bird container 2 */}
-        <div className={`bird-container bird-container-two ${isPlaying ? 'animate' : ''}`}>
-          <div className="bird bird-two"></div>
-        </div>  
-        
-        {/* Bird container 3 */}
-        <div className={`bird-container bird-container-three ${isPlaying ? 'animate' : ''}`}>
-          <div className="bird bird-three"></div>
-        </div> 
-        
-        {/* Bird container 4 */}
-        <div className={`bird-container bird-container-four ${isPlaying ? 'animate' : ''}`}>
-          <div className="bird bird-four"></div>
-        </div>
+  return (
+    <div>
+      {/* Bird container 1 */}
+      <div
+        className={`bird-container bird-container-one ${isPlaying ? "animate" : ""}`}
+      >
+        <div className="bird bird-one"></div>
       </div>
-      
-     );
+
+      {/* Bird container 2 */}
+      <div
+        className={`bird-container bird-container-two ${isPlaying ? "animate" : ""}`}
+      >
+        <div className="bird bird-two"></div>
+      </div>
+
+      {/* Bird container 3 */}
+      <div
+        className={`bird-container bird-container-three ${isPlaying ? "animate" : ""}`}
+      >
+        <div className="bird bird-three"></div>
+      </div>
+
+      {/* Bird container 4 */}
+      <div
+        className={`bird-container bird-container-four ${isPlaying ? "animate" : ""}`}
+      >
+        <div className="bird bird-four"></div>
+      </div>
+    </div>
+  );
 }
--- src/assets/navbar/navbar.tsx
+++ src/assets/navbar/navbar.tsx
@@ -1,32 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { IoIosPause, IoIosPlay } from "react-icons/io";
 
 interface NavbarProps {
-    isMusicPlaying: boolean;
+  isMusicPlaying: boolean;
   onPlayPause: () => void;
   audioSource: string | null;
 }
 
-export default function Navbar({ isMusicPlaying, onPlayPause, audioSource }: NavbarProps) {
-    const [isFoldoutOpen, setFoldoutOpen] = useState(false);
+export default function Navbar({
+  isMusicPlaying,
+  onPlayPause,
+  audioSource,
+}: NavbarProps) {
+  const [isFoldoutOpen, setFoldoutOpen] = useState(false);
 
   return (
     <div className="w-full bg-transparent text-black text-lg">
-      {/* Navbar container */}
+      {/* Navbar div */}
       <div className="relative flex justify-between items-center py-4 px-8">
         {/* Play/Pause Button */}
         <button
-  onClick={onPlayPause}
-  className={`text-2xl ${audioSource === null ? 'text-gray-400 cursor-not-allowed' : ''}`}
-  disabled={audioSource === null}
->
+          onClick={onPlayPause}
+          className={`text-2xl ${audioSource === null ? "text-gray-400 cursor-not-allowed" : ""}`}
+          disabled={audioSource === null}
+        >
           {isMusicPlaying ? <IoIosPause /> : <IoIosPlay />}
         </button>
         <div className="absolute left-1/2 transform -translate-x-1/2">
-        <h1 className="text-black text-2xl font-bold" style={{ letterSpacing: "0.1em" }}>élodie</h1>
+          <h1
+            className="text-black text-2xl font-bold"
+            style={{ letterSpacing: "0.1em" }}
+          >
+            élodie
+          </h1>
         </div>
 
-        {/* Navbar menu items (right aligned) */}
+        {/* Navbar menu */}
         <div className="flex-none ml-auto">
           {/* Foldout Menu with Details */}
           <details
@@ -34,29 +43,38 @@ export default function Navbar({ isMusicPlaying, onPlayPause, audioSource }: Nav
             onToggle={() => setFoldoutOpen((prevState) => !prevState)}
             className="relative"
           >
-            {/* Summary with custom arrow, no default arrow */}
             <summary className="cursor-pointer text-lg text-black list-none font-bold">
               <span className="ml-2">
                 {/* Custom arrow */}
-                {isFoldoutOpen ? '..' : '...'}
+                {isFoldoutOpen ? ".." : "..."}
               </span>
             </summary>
 
-            {/* Foldout Menu Content */}
+            {/* Foldout Menu */}
             <ul
               className={`bg-gray-950 opacity-95 rounded-md z-1 text-gray-300 p-4 shadow-none border-solid border-1 border-black z-50 transition-opacity duration-500 ease-in-out ${
-                isFoldoutOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
+                isFoldoutOpen ? "opacity-100" : "opacity-0 pointer-events-none"
               }`}
               style={{
-                position: 'absolute', 
-                top: '100%', 
-                right: 0, 
-                width: 'max-content', 
-                maxWidth: '100vw', 
+                position: "absolute",
+                top: "100%",
+                right: 0,
+                width: "max-content",
+                maxWidth: "100vw",
               }}
             >
-<li><a href="https://sv.wikipedia.org/wiki/%C3%89lodie" target="_blank" rel="noopener noreferrer">About</a></li>
-<li><a href="#">Gallery</a></li>
+              <li>
+                <a
+                  href="https://sv.wikipedia.org/wiki/%C3%89lodie"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#">Gallery</a>
+              </li>
             </ul>
           </details>
         </div>
