975e757e75236b7f59cb8ac6fdde53325678757f
Andreas <andreas.erik.lundmark@gmail.com>
2025-04-12 21:01:52 +0200

    controller might work

--- backend/audiostream/src/main/java/dev/audiostream/controller/AudioController.java
+++ backend/audiostream/src/main/java/dev/audiostream/controller/AudioController.java
@@ -3,11 +3,14 @@ package dev.audiostream.controller;
 
 import dev.audiostream.service.AudioService;
 import jakarta.annotation.Resource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 @RestController
 @CrossOrigin("*")
@@ -21,10 +24,10 @@ public class AudioController {
     }
 
     @GetMapping("/{fileName}")
-    public ResponseEntity<Resource> playSong(@PathVariable String fileName,
-    @RequestHeader(value = "Range", required = false) String rangeHeader) throws IOException {
+    public ResponseEntity<InputStreamResource> playSong(@PathVariable String fileName,
+                                                        @RequestHeader(value = "Range", required = false) String rangeHeader) throws IOException {
 
-        Resource audioFile = audioService.getFileByName(fileName);
+        FileSystemResource audioFile = (FileSystemResource) audioService.getFileByName(fileName);
         File file = audioFile.getFile();
 
         long fileLength = file.length();
@@ -32,17 +35,35 @@ public class AudioController {
         long rangeEnd = fileLength - 1;
 
         String fileType;
-        String endOfFile = fileName.toLowerCase().substring(fileName.length()-3);
+        String endOfFile = fileName.toLowerCase().substring(fileName.length()-4);
 
-        if(endOfFile.equals("mp3")) {
+        if(endOfFile.equals(".mp3")) {
             fileType = "audio/mpeg";
         }
-        else if(endOfFile.equals("wav")) {
+        else if(endOfFile.equals(".wav")) {
             fileType = "audio/wav";
         }
         else {
             return ResponseEntity.badRequest().build();
         }
-        return null;
+
+        if (rangeEnd >= fileLength) {
+            rangeEnd = fileLength - 1;
+        }
+
+        long contentLength = rangeEnd - rangeStart + 1;
+        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+        inputStream.skip(rangeStart);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", fileType);
+        headers.set("Accept-Ranges", "bytes");
+        headers.set("Content-Length", String.valueOf(contentLength));
+        headers.set("Content-Range", String.format("bytes %d-%d/%d", rangeStart, rangeEnd, fileLength));
+
+        return ResponseEntity.status(rangeHeader != null ? HttpStatus.PARTIAL_CONTENT : HttpStatus.OK)
+                .headers(headers)
+                .body(new InputStreamResource(inputStream));
+
     }
 }
