4a08c9158b31aa5cf962f324cd1c2794e03ae67e
Andreas <andreas.erik.lundmark@gmail.com>
2025-04-13 21:49:49 +0200

    audioservice updated

--- backend/audiostream/src/main/java/dev/audiostream/service/AudioService.java
+++ backend/audiostream/src/main/java/dev/audiostream/service/AudioService.java
@@ -1,15 +1,54 @@
 package dev.audiostream.service;
 
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 @Service
 public class AudioService {
+    
+    @Value("${gcp.bucket.name}")
+    private String bucketName;
 
     public Resource getFileByName(String fileName) {
         String filePath = "audioFiles/" + fileName;
         return new ClassPathResource(filePath);
+
+    }
+
+    /**
+     * Talks to GCP storage API that provides a Blob
+     * @param fileName
+     * @return
+     */
+    public Resource getGcpFileByName(String fileName) {
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        String filePath = "audioFiles/" + fileName;
+
+        Blob blob = storage.get(bucketName, filePath);
+
+        if (blob == null) {
+            throw new IllegalArgumentException("File not found in GCS: " + filePath);
+        }
+
+
+        byte[] content = blob.getContent();
+
+
+        InputStream inputStream = new ByteArrayInputStream(content);
+
+
+        return new InputStreamResource(inputStream);
 
     }
 }
