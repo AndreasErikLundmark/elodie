78426f05c4ad04271697d7d5f22d68bf47365faa
Andreas <andreas.erik.lundmark@gmail.com>
2025-04-23 20:46:26 +0200

    api fetch may not be used

---
+++ src/assets/api/api.tsx
@@ -0,0 +1,29 @@
+// import { ahrefResponseObject, textResponseObject } from "../types/types";
+// const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+const GET_URL = "https://audiostreamer-697604347968.us-central1.run.app/audio/";
+
+export const fetchSong = async (
+  song_name: string, 
+): Promise<string> => {
+ 
+  const urlWithParams = GET_URL+{song_name};
+
+  try {
+    const response = await fetch(urlWithParams, {
+      method: "GET",
+      headers: {
+        "Range": "bytes=0-1023",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch song. Status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching aHrefs:", error);
+    throw error;
+  }
+};
