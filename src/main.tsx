ec54c48b2eb77fda487998094d3277a5ea238075
Andreas <andreas.erik.lundmark@gmail.com>
2025-04-24 09:52:19 +0200

    router install

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+      <Routes>
+      </Routes>
       <App />
+      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )
